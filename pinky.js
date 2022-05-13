091fe6ed542088e430bd30012776a6840fd13442
Das <105265726+Das-x5@users.noreply.github.com>
2022-05-13 13:09:02 +0530

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -5,17 +5,17 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-// PLK Special Functions
+// Das-x5 Special Functions
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 module.exports = {
 
- OA_AI: process.env.Z_PINKY_AI === undefined ? true : process.env.Z_BOTNAME,  
- OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Pinky' : process.env.Z_BOT_NAME,
- OA_NAME: process.env.Z_DEP_NAME === undefined ? 'plk' : process.env.Z_DEP_NAME,
- OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'plk is my creator his full name is afnanplk' : process.env.Z_DEP_REPLY,
+ OA_AI: process.env.Z_TOXIC_AI === undefined ? true : process.env.Z_BOTNAME,  
+ OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Toxic_ser' : process.env.Z_BOT_NAME,
+ OA_NAME: process.env.Z_DEP_NAME === undefined ? 'Das-x5' : process.env.Z_DEP_NAME,
+ OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'Das-x5 is my creator his full name is Asmidas' : process.env.Z_DEP_REPLY,
     
 
 };
